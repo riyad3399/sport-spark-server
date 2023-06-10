0c0edc3d46fdeda03004be85254c93b4db1d4cb7
riyad3399 <programminghero.riyad@gmail.com>
2023-06-10 13:03:01 +0600

    added the get all user api

--- index.js
+++ index.js
@@ -47,6 +47,11 @@ async function run() {
     const usersCollection = client.db("sportDB").collection("users");
 
     // users related api
+    app.get('/users', async (req, res) => {
+      const result = await usersCollection.find().toArray();
+      res.send(result)
+    })
+
     app.post("/users", async (req, res) => {
       const user = req.body;
       const query = { email: user.email };
